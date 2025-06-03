e729b9ebe5e267802d80683753c6784e5ca658b2
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-06-02 21:04:46 -0400

    feat: base del proyecto

---
+++ src/lib/components/Example.svelte
@@ -0,0 +1,8 @@
+<script>
+    let {text} = $props();
+</script>
+
+<div class="example">
+  <h1>{text}</h1>
+  <p>This is an example component.</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Example from "$lib/components/Example.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p><a href="allCards">Todas las cartas </a> to read the documentation</p>
+
+<Example text="Este es un componente de ejemplo" />
