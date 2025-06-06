c5f6f91f763c78c2dbe4eebe886720d7a640ab6d
Bloodbay8 <153459461+Bloodbay8@users.noreply.github.com>
2025-06-06 09:36:37 -0400

    feat: NavHeader Completado, Desktop y Responsive

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import AppBar from "./_components/app-bar.svelte";
+	import NavHeader from "$lib/components/NavHeader.svelte";
 	import Footer from "./_components/footer.svelte";
 
 	let { children } = $props();
 </script>
 
 <div class="flex h-1 min-h-dvh w-full flex-col">
-	<AppBar />
+	<NavHeader />
 	<div class="w-full flex flex-col flex-grow pt-16 sm:pt-24">
 		{@render children()}
 	</div>
