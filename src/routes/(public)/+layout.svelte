d7b365c4591f2f422109369af5449d2238e64d72
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-06-12 19:24:35 -0400

    feat: color de texto por defecto, blanco

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -5,7 +5,7 @@
 	let { children } = $props();
 </script>
 
-<div class="flex h-1 min-h-dvh w-full flex-col">
+<div class="flex h-1 min-h-dvh w-full flex-col text-white">
 	<NavHeader />
 	<div class="w-full flex flex-col flex-grow pt-16 sm:pt-24">
 		{@render children()}
