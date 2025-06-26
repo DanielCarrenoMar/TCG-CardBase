76c7f289f0849d491d44a834ed1f34775d74fd69
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-06-26 17:32:58 -0400

    feat: añadir titulo y ajustar espacios en videojeugos

--- src/routes/(public)/videojuegos/_components/info-card.svelte
+++ src/routes/(public)/videojuegos/_components/info-card.svelte
@@ -3,6 +3,7 @@
   import { Card } from "flowbite-svelte";
   interface Props {
 		info: {
+      title: string;
 			description: string;
 			background_color: string;
 			url: string;
@@ -13,7 +14,8 @@
 </script>
 
 <Card class={`flex flex-col items-center h-auto justify-between text-left shadow-md bg-gradient-to-b ${info.background_color} p-4 sm:p-5 md:p-7`}>
-  <div class="w-full text-white text-2xl mb-6">
+  <div class="w-full text-white text-xl mb-6">
+    <h2 class="text-3xl mb-4" >{info.title}</h2>
     {info.description}
   </div>
   <NavigateButton href={info.url}>Explorar</NavigateButton>
