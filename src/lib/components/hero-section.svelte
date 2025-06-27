01b599b1dc3cbf206e3ea6756cdd514f7c0a9c17
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-06-26 22:08:07 -0400

    feat: hidden en hero-section

--- src/lib/components/hero-section.svelte
+++ src/lib/components/hero-section.svelte
@@ -6,7 +6,7 @@
     let { title, image }: Props = $props();
 </script>
 
-<section class="mx-auto container relative max-h-72">
+<section class="mx-auto container relative max-h-72 overflow-hidden">
 	<div class="absolute inset-0 w-3/5 bg-gradient-to-r from-bg-100/60 via-bg-100/40 via-70% to-bg-100/0 to-100% z-10 flex flex-col justify-center">
 		<div class="w-3/4">
 			<h1 class="text-3xl md:text-5xl text-center font-bold font-title text-white mb-8">{title}</h1>
