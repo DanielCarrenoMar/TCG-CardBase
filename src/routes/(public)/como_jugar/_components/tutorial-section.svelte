3bc3f61c1b0eb97afa9452389a0906a2cf949fc5
Alejandro Rosas <61267550+AlejandroJRosas@users.noreply.github.com>
2025-07-04 17:55:57 -0400

    feat(client): about page form and how to play accordion section

---
+++ src/routes/(public)/como_jugar/_components/tutorial-section.svelte
@@ -0,0 +1,70 @@
+<script>
+	import { AccordionItem, Accordion } from "flowbite-svelte";
+	import { PlusOutline, MinusOutline } from "flowbite-svelte-icons";
+
+	const INFO = [
+		{
+			title: "Nombre de la Carta",
+			description:
+				"En el formato de juego Estándar, puedes tener un máximo de cuatro cartas con el mismo nombre en tu baraja. Las cartas de Energía son la excepción a esta regla.",
+		},
+		{
+			title: "Tipo del Pokémon y PS",
+			description:
+				"Indica el tipo del Pokémon y cuánto daño puede recibir antes de quedar Fuera de Combate. Cada Pokémon tiene su propia Debilidad y Resistencia, recogidas en la parte inferior de la carta. Recuerda esto cuando te prepares para tu próximo combate.",
+		},
+		{
+			title: "Fase",
+			description:
+				"Indica la Evolución actual de tu Pokémon. Puedes hacer evolucionar a aquellos Pokémon que hayan estado en el campo de combate al menos un turno si tienes su forma evolucionada en tu mano.",
+		},
+		{
+			title: "Ataques",
+			description:
+				"Para llevar a cabo un ataque, debes tener unida al Pokémon la cantidad y el tipo de Energía que se indica aquí. El número a la derecha muestra cuánto daño hará el ataque al Pokémon Activo del rival.",
+		},
+		{
+			title: "Coste de Retirada",
+			description:
+				"Esta es la cantidad de Energía necesaria para retirar a tus Pokémon del Puesto Activo a la Banca. Para realizar esta acción, debes descartar dicha cantidad de Energía del Pokémon Activo y sustituirlo por un Pokémon de tu Banca.",
+		},
+	];
+</script>
+
+<main>
+	<section class="flex flex-col">
+		<div class="flex p-12 justify-around">
+			<div class="bg-[#fbf5e4] rounded-lg p-4 w-1/2 my-4">
+				<Accordion class="border-none">
+					{#each INFO as item}
+						<AccordionItem
+							transitionParams={{ duration: 300 }}
+							class="border-transparent border-b-1 border-b-[#dcd4be]"
+							contentClass="border-transparent border-b-1 border-b-[#dcd4be]"
+						>
+							{#snippet header()}{item.title}{/snippet}
+							{#snippet arrowup()}
+								<MinusOutline class="-me-0.5 h-6 w-6" />
+							{/snippet}
+							{#snippet arrowdown()}
+								<PlusOutline class="-me-0.5 h-6 w-6" />
+							{/snippet}
+							<p class="mb-2 text-gray-500 dark:text-gray-400">
+								{item.description}
+							</p>
+						</AccordionItem>
+					{/each}
+				</Accordion>
+			</div>
+
+			<img src="/images/how-to-play/howtoplay.png" alt="Como Jugar" />
+		</div>
+		<a
+			class="py-4 px-8 bg-[#fee732] text-black font-semibold rounded cursor-pointer"
+			href="https://tcg.pokemon.com/es-es/learn/"
+			target="_blank"
+		>
+			Link de Instrucciones
+		</a>
+	</section>
+</main>
--- src/routes/(public)/nosotros/+page.svelte
+++ src/routes/(public)/nosotros/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import HeroSection from '$lib/components/Hero-section.svelte';
+	import HeroSection from "$lib/components/Hero-section.svelte";
+	import Input from "$lib/components/input.svelte";
 	import MemberCard from "./_components/member-card.svelte";
 	import TechCard from "./_components/tech-card.svelte";
 	import Hero from "./_images/hero.png";
@@ -8,22 +9,19 @@
 
 <HeroSection title="Acerca de Nosotros" image={Hero} />
 
-<main
-	class="text-white bg-gradient-to-b from-bg-100 via-bg-300 to-bg-100"
->
+<main class="text-white bg-gradient-to-b from-bg-100 via-bg-300 to-bg-100">
 	<div class="mx-auto container flex flex-col flex-grow">
 		<section class="p-8 flex flex-col gap-2">
 			<h2 class="text-2xl font-bold">Resumen</h2>
 			<p>
-				Nuestra plataforma web está diseñada para revolucionar la
-				experiencia de las catas de cartas TCG Pokémon. Ofrecemos un
-				espacio digital donde los coleccionistas y jugadores pueden
-				participar en catas virtuales, descubrir nuevas cartas,
-				intercambiar con otros usuarios y mantenerse al día con las
-				últimas novedades del mundo Pokémon. Con herramientas avanzadas
-				de análisis, sistema de valoración en tiempo real y una
-				comunidad activa, creamos la experiencia definitiva para los
-				fanáticos de las cartas Pokémon.
+				Nuestra plataforma web está diseñada para revolucionar la experiencia de
+				las catas de cartas TCG Pokémon. Ofrecemos un espacio digital donde los
+				coleccionistas y jugadores pueden participar en catas virtuales,
+				descubrir nuevas cartas, intercambiar con otros usuarios y mantenerse al
+				día con las últimas novedades del mundo Pokémon. Con herramientas
+				avanzadas de análisis, sistema de valoración en tiempo real y una
+				comunidad activa, creamos la experiencia definitiva para los fanáticos
+				de las cartas Pokémon.
 			</p>
 		</section>
 
@@ -51,7 +49,42 @@
 
 		<section class="p-8 flex flex-col gap-6">
 			<h2 class="text-2xl font-bold">Contacta con nosotros</h2>
-			<form class="flex flex-col"></form>
+			<form>
+				<div class="grid grid-cols-2 gap-2">
+					<div class="flex flex-col">
+						<Input
+							name="name"
+							type="text"
+							class="input input-bordered"
+							label="Nombre"
+						/>
+						<Input
+							name="email"
+							type="email"
+							class="input input-bordered"
+							label="Correo electrónico"
+						/>
+						<Input
+							name="topic"
+							type="text"
+							class="input input-bordered"
+							label="Asunto"
+						/>
+					</div>
+					<Input
+						name="message"
+						type="textarea"
+						class="input input-bordered"
+						label="Mensaje"
+					/>
+				</div>
+				<button
+					type="submit"
+					class="cursor-pointer py-2 px-4 bg-primary-700 rounded"
+				>
+					Enviar
+				</button>
+			</form>
 		</section>
 	</div>
 </main>
